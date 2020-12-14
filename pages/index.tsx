d711fb204e7fc71d6ebf7bbceb9bcb58d286c5fc
Swapnil Sengupta <dodosp2001@gmail.com>
2020-12-14 21:21:26 +0530

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import styles from './index.module.scss'
+import { useState } from 'react'
+import jwt from 'jsonwebtoken'
+
+export default function Home() {
+  const [password, setPassword] = useState<string>('')
+  const [username, setUsername] = useState<string>('')
+  const [message, setMessage] = useState<string>('You are not logged in.')
+  async function submitForm() {
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    }).then(t => t.json())
+    const token = res.token
+    if (token) {
+      const json = jwt.decode(token) as { [key: string]: string }
+      setMessage(`Welcome ${json.username} and you are ${json.admin ? 'an admin' : 'not an admin'}!`)
+    } else {
+      setMessage('Something went wrong!')
+    }
+  }
+  return (
+    <div>
+      <h1>{message}</h1>
+      <form method="POST" action="/api/login">
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)} />
+        <br />
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)} />
+        <br />
+        <input type="button" value="login" onClick={submitForm}/>
+      </form>
+    </div>
+  )
+}
